6cad5ffdc131b9fa6d58ab478f2687d274132018
sudhachalla <30652197+sudhachalla@users.noreply.github.com>
2018-07-30 17:10:58 +0530

    new test
    new test

---
+++ profile/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() { 
+	$('#fullpage').fullpage({
+		sectionsColor: ['#ffffff', 'orange', '#333333', '#d1055e'],
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+		menu: '#menu',
+	}); 
+
+
+})
